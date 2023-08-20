62446a02892dcbe24806de5c5d5e3b2101c30504
Aryan26-02 <142403141+Aryan26-02@users.noreply.github.com>
2023-08-20 15:40:44 +0530

    Add files via upload

---
+++ circumferenceofcircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // circumference of circle = 2*pi*radius
+    float r = 10;
+    float pi = 3.1415;
+    float c = 2 * pi * r; // c= circumference of circle
+    cout << c;
+}
